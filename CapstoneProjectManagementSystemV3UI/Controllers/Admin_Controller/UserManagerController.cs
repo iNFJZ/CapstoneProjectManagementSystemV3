9be54bf4df3f2217c50b8084ac464ad74fd286b2
tuanpnc <bogia1403@gmail.com>
2025-03-06 22:42:33 +0700

    commit fron end admin

---
+++ CapstoneProjectManagementSystemV3UI/Controllers/Admin_Controller/UserManagerController.cs
@@ -0,0 +1,38 @@
+﻿using Infrastructure.Services.CommonServices.SessionExtensionService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CapstoneProjectManagementSystemV3UI.Controllers.Admin_Controller
+{
+   
+    public class UserManagerController : Controller
+    {
+        private readonly ISessionExtensionService _sessionExtensionService;
+        private readonly ILogger<UserManagerController> _logger;
+        private readonly IConfiguration _configuration;
+        public UserManagerController(ISessionExtensionService sessionExtensionService
+     , IConfiguration configuration
+                         , ILogger<UserManagerController> logger)
+        {
+            _sessionExtensionService = sessionExtensionService;
+            _configuration = configuration;
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            try
+            {
+                //    _logger.LogInformation("View Create User Screen");
+                //    User user = _sessionExtensionService.GetObjectFromJson<User>(HttpContext.Session, "sessionAccount");
+
+                //    ViewBag.sessionRole = user.Role.RoleId;
+                return View("Views/Admin_View/UserManager/Index.cshtml");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "View Create User Screen error");
+                return View("/Views/Common_View/404NotFound.cshtml");
+            }
+        }
+    }
+}
